ab6cbe32c3f91137c410942e3b70598973330c9e
Maryann Ndungu <marieshi91@gmail.com>
2022-12-03 14:06:34 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x){
+    return x < 42 ? -(x-42) : x-42;
+}
+console.log (distanceFromHqInBlocks(34));
+
+function distanceFromHqInFeet(x) {
+ return distanceFromHqInBlocks(x)*264
+}
+console.log (distanceFromHqInFeet(34));
+
+function distanceTravelledInFeet(a,b){
+    return b<a ? -((b-a)*264) : (b-a)*264
+}
+console.log (distanceTravelledInFeet(48,43))
+
+function calculatesFarePrice(a,b){
+    if (distanceTravelledInFeet(a,b) <= 400) {
+    return 0 
+    }
+    else if (distanceTravelledInFeet(a,b) <=2000) {
+        return ((distanceTravelledInFeet(a,b))-400)*0.02
+    }
+    else if (distanceTravelledInFeet(a,b) >2000<=2500){
+        return 25
+}
+else {(distanceTravelledInFeet(a,b)>2500) 
+ return 'cannot travel that far'}
+}
+console.log (calculatesFarePrice(34,24))
